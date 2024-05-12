615d78ce3f1012a3779b2adc76f4f435e698a4b7
Nahid5h <nahidhassan02022002@gmail.com>
2024-05-12 16:48:24 +0600

    get sing data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app=express();
 const port =process.env.PORT || 5000;
@@ -34,6 +34,13 @@ async function run() {
 
       res.send(result)
 
+    })
+    // get a single job data 
+    app.get('/room/:id', async(req,res)=>{
+      const id =req.params.id
+      const query={_id:new ObjectId(id)}
+      const result = await roomsCollection.findOne(query)
+      res.send(result)
     })
     
 
