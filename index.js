ec73a8049c9552c5dd0342254de19f0d51d7b6e3
Nahid5h <nahidhassan02022002@gmail.com>
2024-05-12 09:30:11 +0600

    get data from mongodb

--- index.js
+++ index.js
@@ -24,14 +24,28 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const roomsCollection=  client.db('hotelBooking').collection('rooms')
+    const dataCollection=  client.db('hotelBooking').collection('data')
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    // get all  rooms from bd 
+    app.get("/featured", async(req,res)=>{
+      const result = await roomsCollection.find().toArray()
+
+      res.send(result)
+
+    })
+    
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
